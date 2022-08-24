aabf2ba8089e43fe42b306ba7058c1e0c3959aec
dd301 <donatodoley1@gmail.com>
2022-08-24 20:02:44 +0530

    made a working input box and button

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,18 +1,28 @@
 var main = function () {
 "use strict";
-
-    var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&format=json&jsoncallback=?";
-    $.getJSON(url, function(flickrResponse){
-        flickrResponse.items.forEach(function(photo){
-
-            var $img = $("<img>").hide();
-            $img.attr("src", photo.media.m);
-            $("main .photos").append($img);
-            $img.fadeIn();
-        });
-    });
     
+    var displayImages = function(tag){
+        
+        var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags="+ tag +"&format=json&jsoncallback=?";
+        $.getJSON(url, function(flickrResponse){
+            flickrResponse.items.forEach(function(photo){
+                var $img = $("<img>").hide();
+                $img.attr("src", photo.media.m);
+                $("main .photos").append($img);
+                $img.fadeIn();
+            });
+        });
 
-};
+    };
+    
+        
+    $(".tag-input button").on("click", function(event){
+        if ($(".tag-input input").val() !== ""){
+            $("main .photos").empty();
+            displayImages($(".tag-input input").val());
+        }
+    });
+
+}
 
 $(document).ready(main);
